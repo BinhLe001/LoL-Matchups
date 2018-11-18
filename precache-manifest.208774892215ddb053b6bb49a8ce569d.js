682a5b3d606284f3e51080773c1b04ede320a7db
Binh Le <binhle@mit.edu>
2018-11-18 02:54:18 -0500

    Updates

---
+++ precache-manifest.208774892215ddb053b6bb49a8ce569d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b470daee5d81351092ca",
+    "url": "/LoL-Matchups/static/js/runtime~main.b470daee.js"
+  },
+  {
+    "revision": "4e884c29d9545aec0af7",
+    "url": "/LoL-Matchups/static/js/main.4e884c29.chunk.js"
+  },
+  {
+    "revision": "36fab3224b025958673c",
+    "url": "/LoL-Matchups/static/js/1.36fab322.chunk.js"
+  },
+  {
+    "revision": "4e884c29d9545aec0af7",
+    "url": "/LoL-Matchups/static/css/main.5ab932f1.chunk.css"
+  },
+  {
+    "revision": "141530cf425f0923da3231c1c14a6723",
+    "url": "/LoL-Matchups/index.html"
+  }
+];
